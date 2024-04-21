cabc72d2f997c5208139ccd5ae32ba68da5f8ebd
Nahuel Aguirre MacBook Pro <nahuelaguirre@Nahuels-MacBook-Pro.local>
2024-04-20 19:40:57 -0600

    Ajuste hecho por DANI en html y js desde Drive y actualizado en GIT

--- js/script.js
+++ js/script.js
@@ -1,1 +1,39 @@
 //Este espacio es para escribir el JS y darle funcion a la calculadora
+
+let operador = '';
+let valorAnterior = 0;
+let valorActual = 0;
+
+function agregarNumero(numero) {
+    valorActual = valorActual * 10 + numero;
+    actualizarPantalla();
+}
+
+function operacion(op) {
+    operador = op;
+    valorAnterior = valorActual;
+    valorActual = 0;
+}
+
+function raiz() {
+    valorActual = Math.sqrt(valorActual);
+    actualizarPantalla();
+}
+
+function calcular() {
+    if (operador === '+') {
+        valorActual = valorAnterior + valorActual;
+    } else if (operador === '-') {
+        valorActual = valorAnterior - valorActual;
+    } else if (operador === '*') {
+        valorActual = valorAnterior * valorActual;
+    } else if (operador === '/') {
+        valorActual = valorAnterior / valorActual;
+    }
+    actualizarPantalla();
+    valorAnterior = 0;
+}
+
+function actualizarPantalla() {
+    document.getElementById('pantalla').textContent = valorActual;
+}
